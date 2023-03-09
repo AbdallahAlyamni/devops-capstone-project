25a3dd91fb93901ba3a49329ea17c28ddcb41431
AbdallahAlyamni <AbdallahAlyamni@gmail.com>
2023-03-09 10:26:29 -0500

    added link task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -181,7 +181,7 @@ class TestAccountService(TestCase):
         """It should not allow an illegal method call"""
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-    
+
     def test_security_headers(self):
         """It should return security headers"""
         response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
@@ -195,7 +195,7 @@ class TestAccountService(TestCase):
         }
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
-    
+
     def test_cors_policies(self):
         """It should return cors header"""
         response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
@@ -208,4 +208,3 @@ class TestAccountService(TestCase):
             response.headers.get('Access-Control-Allow-Origin'),
             headers['Access-Control-Allow-Origin']
         )
-
